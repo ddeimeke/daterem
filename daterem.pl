013879d3f77dd4796511a32f6d794a4f83d6a27b
Dirk Deimeke <dirk@deimeke.net>
2012-04-10 14:02:45 +0200

    Additional check

--- daterem.pl
+++ daterem.pl
@@ -32,8 +32,8 @@ sub options
 		$month++;
 		$year += 1900;
 	}
-	$month = "0" . $month if ($month < 10);
-	$day = "0" . $day if ($day < 10);
+	$month = "0" . $month if (defined $month) and ($month < 10);
+	$day = "0" . $day if (defined $day) and ($day < 10);
 	return ($day,$month,$year);
 }
 ##############
