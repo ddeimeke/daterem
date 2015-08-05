09a0f3c75c795eed7b3d71fb5af763c78b8ba025
Dirk Deimeke <dirk@deimeke.net>
2015-08-05 09:26:47 +0200

    Some source "beautifying".

--- daterem.py
+++ daterem.py
@@ -23,6 +23,7 @@ def options():
 
     if len(sys.argv)==2:
         list = sys.argv[1].split(".")
+
         if len(list)==1:
             year  = list[0]
             month = ''
@@ -35,10 +36,13 @@ def options():
             year  = list[2]
             month = list[1]
             day   = list[0]
-        if len(day)==1:
+
+        if len(day) == 1:
             day   = '0' + day
-        if len(month)==1:
+
+        if len(month) == 1:
             month = '0' + month 
+
     else:
         day   = time.strftime("%d")
         month = time.strftime("%m")
@@ -83,70 +87,82 @@ def calceaster(): # Calculate easter date
     return to_epoch("%s.%s.%s" % (str(o), str(n), str(j)))
 
 
-###########################
-def add2list(date, remark):
-###########################
-    alldates.append(date + " " + remark)
-
-
 ##############
 def readdat():
 ##############
     f = open('daterem.dat', 'r')
     for line in f:
         line = line.strip()
+
         if not re.match("#", line):
-            line = re.sub(r'\s{2,}',' ',line)
+            line              = re.sub(r'\s{2,}',' ',line)
             time, description = line.split(" ",1)
+
             if re.match("^[-0-9]+$", time): # Datum abhaengig von Ostern
                 time = int(time) * day + easter
-                add2list(to_date(time), description)
+                alldates.append("%s %s" % (to_date(time), description))
             elif re.match(".+-.+", time):   # Zeitraeume
                 description = description + " [" + time + "]"
-                dat1, dat2 = time.split("-")
-                dat1list = dat1.split(".")
-                if len(dat1list)==2:
+                dat1, dat2  = time.split("-")
+                dat1list    = dat1.split(".")
+                dat2list    = dat2.split(".")
+
+                if len(dat1list) == 2:
                     dat1list.append('')
-                dat2list = dat2.split(".")
-                if len(dat2list)==2:
+
+                if len(dat2list) == 2:
                     dat2list.append('')
-                if dat2list[2]=='':
+
+                if dat2list[2] == '':
                     dat2list[2]=ryear
-                if dat1list[2]=='':
+
+                if dat1list[2] == '':
                     dat1list[2] = dat2list[2]
-                    if dat1list[1]=='':
+
+                    if dat1list[1] == '':
                         dat1list[1] = dat2list[1]
+
                 dat1 = to_epoch(dat1list[0] + "." + dat1list[1] + "." +  dat1list[2])
                 dat2 = to_epoch(dat2list[0] + "." + dat2list[1] + "." +  dat2list[2])
+
                 while dat1 <= dat2:
-                    add2list(to_date(dat1),description)
+                    alldates.append("%s %s" % (to_date(dat1), description))
                     dat1 = dat1 + day
+
             else:                           # Ein einzelnes Datum
                 timelist = time.split(".")
                 timelist.append('')
+
                 if timelist[2] == '':
                     timelist[2] = ryear
-                add2list("%s.%s.%s" % (timelist[0], timelist[1], timelist[2]), description)
+
+                alldates.append("%s.%s.%s %s" % (timelist[0], timelist[1], timelist[2], description))
     f.close()
 
 
-#######################
+######################
 def printline( line ):
-#######################
+######################
     date, text = line.split(" ",1)
-    prt =  time.strftime("%a, %d.%m.%Y", time.localtime(to_epoch(date))) + ", " + text
-    match = re.match(r".*(born|dead|started|year) (\d{4})",text)
+    prt        = time.strftime("%a, %d.%m.%Y", time.localtime(to_epoch(date))) + ", " + text
+    match      = re.match(r".*(born|dead|started|year) (\d{4})",text)
+
     if match:
+
         if match.group(1) in ('born','year'):
             prt += ", age "
         elif match.group(1) in ('dead', 'started'):
             prt += ", ago: "
-        age = int(ryear) - int(match.group(2))
+
+        age  = int(ryear) - int(match.group(2))
         prt += str(age) + " year"
+
         if age>1:
             prt += "s"
+
     if re.search(", countdown",line):
         duration = int((to_epoch(date) - to_epoch(time.strftime("%d.%m.%Y"))) / day + 0.5)
+
         if duration == 0:
             countdown = " today"
         elif duration == 1:
@@ -155,10 +171,12 @@ def printline( line ):
             countdown = " in %s days" % duration
         else:
             countdown = ""
-            prt = ""
+            prt       = ""
+
         prt = re.sub(' countdown,', countdown, prt)
         prt = re.sub(' countdown', countdown, prt)
-    if prt <> "":
+
+    if prt != "":
         print(prt)
 
 
